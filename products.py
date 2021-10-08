fb2af193d1d33421e18c240400e25977e92741ab
yvette chen <annd840214@gmail.com>
2021-10-08 14:26:18 +0800

    add header

--- products.py
+++ products.py
@@ -21,6 +21,12 @@ for p in products:
 
 for p in products:
     print(p[0], "的價格是", p[1])
-    
-    
+
+
+with open("products.csv", "w", encoding = "utf-8") as f:
+    f.write("商品,價格\n")
+    for p in products:
+        f.write(p[0] + "," + str(p[1]) + "\n")
+
+
     

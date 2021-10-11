3fd9037e784222006c72860ccbb7d86c25db0659
yvette chen <annd840214@gmail.com>
2021-10-11 11:26:04 +0800

    讀取與寫入update

--- products.py
+++ products.py
@@ -9,7 +9,7 @@ with open("products.csv", "r", encoding = "utf-8") as f:
 
 print(products)
 
-
+#讓使用者輸入
 while True:
     name = input("請輸入商品名稱：")
     if name == "q":
@@ -23,16 +23,12 @@ while True:
     products.append(little_products)
 print(products)
 
-for p in products:
-    print(p)
 
-for p in products:
-    print(p[0]) #只印出商品名字
-
+#印出所有購買紀錄
 for p in products:
     print(p[0], "的價格是", p[1])
 
-# 寫入檔案
+#寫入檔案
 with open("products.csv", "w", encoding = "utf-8") as f:
     f.write("商品,價格\n")
     for p in products:
